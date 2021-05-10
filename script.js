987887aae041c8aeca4749c2c2c1c2c710f2256c
unequalv <unequalv@gmail.com>
2021-05-10 12:09:22 +0530

    Final game in console

--- script.js
+++ script.js
@@ -11,34 +11,43 @@ function computerplay() {
 
 function pp() {
     let pp = prompt("Choose");
+    let ap = pp.toLowerCase();
     let ppno;
+    if (ap == "rock") { ppno = 0; }
+    if (ap == "paper") { ppno = 1; }
+    if (ap == "scissor") { ppno = 2; }
 
-    pp = pp.toLowerCase;
-    if (pp == "rock")
-        ppno = 0;
-    else if (pp == "paper")
-        ppno = 1;
-    else if (pp == "scissor")
-        ppno = 2;
     return ppno;
 }
 
 function playround(cp, ppno) {
-    if (cp == ppno)
-        console.log("Draw")
+    if (cp == ppno) {
+        console.log("Draw");
+        return 0;
+    }
     else if ((cp == 0 && ppno == 1) || (cp == 1 && ppno == 2) || (cp == 2 && ppno == 0)) {
 
-        console.log("Win")
+        console.log("Win");
+        return 1;
     }
-    else
-        console.log("Lose")
+    else {
+
+        console.log("Lose");
+        return - 1;
+    }
 }
 function game() {
     let i;
+    let count = 0;
     for (i = 0; i < 5; i++) {
-        let cp = computerplay();
         let ppno = pp();
-        playround(cp, ppno);
+        let cp = computerplay();
+        switch (playround(cp, ppno)) {
+            case 1:
+                count++;
+        }
+
     }
+    console.log(count);
 }
 game();
